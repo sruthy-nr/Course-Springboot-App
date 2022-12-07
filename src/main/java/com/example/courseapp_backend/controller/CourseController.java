aa60e66cdba9b79538e3c821cf5c30ac0978c0b1
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-07 11:05:08 +0530

    development

--- src/main/java/com/example/courseapp_backend/controller/CourseController.java
+++ src/main/java/com/example/courseapp_backend/controller/CourseController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 public class CourseController {
+
     @Autowired
     private CourseDao dao;
+    @CrossOrigin(origins = "*")
     @GetMapping("/")
     public String Homepage(){
         return "Welcome to my website";
@@ -17,17 +19,19 @@ public class CourseController {
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
     public String AddCourse(@RequestBody Courses c){
+
         System.out.println(c.getCourseTitle().toString());
         System.out.println(c.getCourseDescription().toString());
         System.out.println(c.getCourseVenue().toString());
         System.out.println(c.getCourseDuration().toString());
         System.out.println(c.getCourseDate().toString());
         dao.save(c);
+
         return "Course added successfully";
     }
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
     public List<Courses> ViewCourses(){
-        return (List<Courses>)dao.findAll() ;
+        return (List<Courses>) dao.findAll();
     }
 }
