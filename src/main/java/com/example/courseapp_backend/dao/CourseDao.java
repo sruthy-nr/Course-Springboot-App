42075442fcfb6fe20bc3edb7cc4ba1e677c18988
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-07 13:48:20 +0530

    development

--- src/main/java/com/example/courseapp_backend/dao/CourseDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.courseapp_backend.dao;
-
-import com.example.courseapp_backend.model.Courses;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseDao extends CrudRepository<Courses,Integer> {
-}
