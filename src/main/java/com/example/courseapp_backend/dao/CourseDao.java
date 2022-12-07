f8cbe1f2eebce5138bf7bf793a637b5039575776
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-07 17:00:53 +0530

    search by like

--- src/main/java/com/example/courseapp_backend/dao/CourseDao.java
+++ src/main/java/com/example/courseapp_backend/dao/CourseDao.java
@@ -8,6 +8,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CourseDao extends CrudRepository<Courses,Integer> {
-    @Query(value = "SELECT `id`, `course_date`, `course_description`, `course_duration`, `course_title`, `course_venue` FROM `courses` WHERE `course_title`=:coursetitle",nativeQuery = true)
+    @Query(value = "SELECT `id`, `course_date`, `course_description`, `course_duration`, `course_title`, `course_venue` FROM `courses` WHERE `course_title` LIKE %:coursetitle%",nativeQuery = true)
     List<Courses> SearchCourse(@Param("coursetitle") String coursetitle);
 }
